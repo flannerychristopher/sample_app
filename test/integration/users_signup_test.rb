55b8be07af820b745a12694251b8f1231d3c8ff0
Chris Flan <flanthedev@gmail.com>
2017-06-11 13:38:21 +1000

    Revert "ch10 and exercises complete"
    This reverts commit 016d6572b3769a6f45e862f2b1086ae5d01239c2.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -34,17 +34,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     #try to log in before activation
     log_in_as(user)
     assert_not is_logged_in?
-    #index page
-    # log in as valid user.
-    log_in_as(users(:michael))
-    # unactivated user is on the second page
-    get users_path, params: { page: 2 }
-    assert_no_match user.name, response.body
-    # profile page
-    get user_path(user)
-    assert_redirected_to root_url
-    # log out valid user
-    delete logout_path
     #invalid activation token
     get edit_account_activation_path("invalid token")
     assert_not is_logged_in?
